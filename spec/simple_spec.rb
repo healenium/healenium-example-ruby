24d3b1a8a42781bd2c811525c2e91cd95b9499ea
alerof <alaerof@gmail.com>
2026-03-31 13:12:03 +0300

    Update locators based on Healenium report

--- spec/simple_spec.rb
+++ spec/simple_spec.rb
@@ -15,34 +15,33 @@ RSpec.describe 'Simple Locator API Tests' do
   end
 
   it 'Update locator for element with css id' do
-    id_element = @driver.find_element(:id, 'change_id')
+    id_element = @driver.find_element(:css, 'input#newValue')
     expect(id_element.displayed?).to be true
 
     click_submit
 
-    change_id_element = @driver.find_element(:id, 'change_id')
+    change_id_element = @driver.find_element(:css, 'input#newValue')
     expect(change_id_element.displayed?).to be true
   end
 
   # :enabled is a CSS pseudo-class; Selenium has no separate :enabled strategy—use :css (or :xpath).
   it 'Update locator for element with css Enabled' do
-    enabled_element = @driver.find_element(:css, 'textarea:enabled')
+    enabled_element = @driver.find_element(:css, 'textarea#change_enabled')
     expect(enabled_element.displayed?).to be true
 
     click_submit
 
-    changed_enabled_element = @driver.find_element(:css, 'textarea:enabled')
+    changed_enabled_element = @driver.find_element(:css, 'textarea#change_enabled')
     expect(changed_enabled_element.displayed?).to be true
   end
 
   it 'XPath Not Contains' do
-    xpath = '//input[not(contains(@class, "input1")) and contains(@class, "test_class")]'
-    not_contains_element = @driver.find_element(:xpath, xpath)
+    not_contains_element = @driver.find_element(:css, 'input#change_className')
     expect(not_contains_element.displayed?).to be true
 
     click_submit
 
-    changed_not_contains_element = @driver.find_element(:xpath, xpath)
+    changed_not_contains_element = @driver.find_element(:css, 'input#change_className')
     expect(changed_not_contains_element.displayed?).to be true
   end
 end
